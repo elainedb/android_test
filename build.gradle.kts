0b14361d6cbd2cbe9fa700ff919b344a1c79e056
elaine <elainedb.dev@gmail.com>
2025-09-14 18:42:46 +0200

    add jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,18 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("org.sonarqube") version "6.3.1.5724"
+    id "jacoco"
 }
 
 sonar {
     properties {
         property("sonar.projectKey", "elainedb_android_test")
         property("sonar.organization", "elainedb")
+    }
+}
+
+jacocoTestReport {
+    reports {
+        xml.required = true
     }
 }
